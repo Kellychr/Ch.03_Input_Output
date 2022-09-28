bf97f2e2a6e2f643175de2475db6ceaacd6afbba
kellychr <kellychr@urbandaleschools.com>
2022-09-28 08:22:03 -0500

    Ch. 03 Jedi Training

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,14 +1,14 @@
-k# Sign your name:________________
-# In all the short programs below, do a good job communicating with your end user!
+# # Sign your name:________________
+# # In all the short programs below, do a good job communicating with your end user!
+#
+#
+#
+#
+# # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
+name=input("what is your name")
+print("hello",name,"how are you doing today")
 
-
-
-
-# 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-#name=input("what is your name")
-#print("hello"name,"how are you doing today")
-
-# 2. Write a program where a user enters a base and height and you print the area of a triangle.
+# # 2. Write a program where a user enters a base and height and you print the area of a triangle.
 int()
 
 base= float(input("what is the base of the triangle in cm"))
@@ -19,28 +19,28 @@ area=base*height/2
 print("the area of the triangle was",area,"Cm^2")
 import math
 
+#
+# # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
+r=float(input("what is the radius of the circle"))
 
-# 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-# r=float(input("what is the radius of the circle"))
-#
-# c=2*3.14*r
-#
-# print("the circumference of your circle is",c,"meters")
+c=2*3.14*r
+
+print("the circumference of your circle is",c,"meters")
 
 
 # 4. Ask a user for an integer and then print the square root.
-# num=int(input("enter an integer:"))
-#
-# sr=math.sqrt(num)
-#
-# print("the squareroot of",num,"is",sr)
+num=int(input("enter an integer:"))
+
+sr=math.sqrt(num)
+
+print("the squareroot of",num,"is",sr)
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
 
-# m=(input("what is the mass of the object in kg?")
-#
-# h=(input("what is the acceleration of the object in m/s^2"))
-#
-#
-#  print("may the",m*a,"Newtons be wiht you!")
-# print("get it")
+m=float(input("what is the mass of the object in kg?"))
+
+a=float(input("what is the acceleration of the object in m/s^2"))
+
+
+print("may the",m*a,"Newtons be with you!")
+print("get it")
