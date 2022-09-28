5cd457e13e98dd7c1c569f848e2642a149914552
kellychr <kellychr@urbandaleschools.com>
2022-09-28 08:09:25 -0500

    final

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -2,20 +2,45 @@
 # In all the short programs below, do a good job communicating with your end user!
 
 
-# 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 
+
+# 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
+#name=input("what is your name")
+#print("hello"name,"how are you doing today")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
+int()
+
+base= float(input("what is the base of the triangle in cm"))
+
+height= float(input("what is the height in cm"))
+
+area=base*height/2
+print("the area of the triangle was",area,"Cm^2")
+import math
 
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
+# r=float(input("what is the radius of the circle"))
+#
+# c=2*3.14*r
+#
+# print("the circumference of your circle is",c,"meters")
 
 
 # 4. Ask a user for an integer and then print the square root.
-
-
+# num=int(input("enter an integer:"))
+#
+# sr=math.sqrt(num)
+#
+# print("the squareroot of",num,"is",sr)
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
 
-
-
+# m=(input("what is the mass of the object in kg?")
+#
+# h=(input("what is the acceleration of the object in m/s^2"))
+#
+#
+#  print("may the",m*a,"Newtons be wiht you!")
+# print("get it")
--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,16 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+#A=a+b/2*h
+
+print("this is a program that helps with finding the area of a trapezoid")
+
+a=float(input("what is the first base"))
+
+b=float(input("what is the second base"))
+
+h=float(input("what is the height of the trapezoid"))
+
+c=((a+b)/2)*h
+
+print(c)
